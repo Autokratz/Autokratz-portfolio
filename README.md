0227bece808a0e65576085d65dfee34f41272cf7
Hector Cabra <hectorcabra95@gmail.com>
2026-09-24 13:25:05 +1000

    Revert "Replace the 2023 React portfolio with the measured case-study site"
    This reverts commit a8a190473a58ca871931c819697c635a4c968d57.

---
+++ README.md
@@ -0,0 +1,26 @@
+## Description
+
+My portfolio, Bootstrapped, created with yarn and with a contentOption to make everything easily adapted without touching any other file, way too easy to customize!
+
+## Author 
+
+**Autokratz**
+
+* [LinkedIn](linkedin.com/in/hectorcabra)
+
+* [Web Portfolio](https://autokratz.github.io/Autokratz-portfolio/)
+
+## Check out the project!
+
+- https://autokratz.github.io/Autokratz-portfolio/
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+### `npm start`
+
+## Contact
+
+You cant text me at hectorcabra95@gmail.com
---
+++ src/content_option.js
@@ -0,0 +1,126 @@
+const logotext = "Autokratz";
+const meta = {
+    title: "Hector Cabra",
+    description: "I am a full stack developer and software engineer. React Enthusiast. Living in Sydney",
+};
+
+const introdata = {
+    title: "I’m Hector Cabra",
+    animated: {
+        first: "Also known as Autokratz",
+        second: "I code cool websites",
+        third: "I like React",
+    },
+    description: "I am a full stack developer and software engineer. React Enthusiast",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "Just a bit about myself",
+    aboutme: "Full stack developer, front end enthusiast. Nucamp full stack development Bootcamp graduate and professional software engineer.",
+};
+const worktimeline = [{
+        jobtitle: "Zcial",
+        where: "Data Analist",
+        date: "2016-2019",
+    },
+   
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Bootstrap",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "React Native",
+        value: 60,
+    },
+    {
+        name: "MongoDB",
+        value: 70,
+    },
+    {
+        name: "Express",
+        value: 60,
+    },
+    {
+        name: "NodeJS",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Politecnico GranColombiano",
+        description: "Bachelor of Software Engineering (2019-2023)",
+    },
+    {
+        title: "Nucamp Bootcamp",
+        description: "Full stack developer bootcamp  (2022-2023)",
+    },
+    
+];
+
+const dataportfolio = [{
+    img: "https://media.giphy.com/media/27C5XriG9nS2QZZSKw/giphy.gif",
+    description: "Stylish tetris game on React!.",
+    link: "https://autokratz.github.io/biotetris-react/",
+    },
+    {
+        img: "https://media.giphy.com/media/PQkjFUqskF4SrVn0cZ/giphy.gif",
+        description: "Stylish Calculator on React!",
+        link: "https://autokratz.github.io/calculator-react/",
+    },
+    {
+        img: "https://media.giphy.com/media/cRguuzx2Zda6q4DYOB/giphy.gif",
+        description: "Stylish To-do list on React!",
+        link: "https://autokratz.github.io/todolist-react/",
+    },
+   
+   
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hectorcabra95@gmail.com",
+    YOUR_FONE: "+61 466799234",
+    description: "Let me know ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_rfg398v",
+    YOUR_TEMPLATE_ID: "template_x6in6qe",
+    YOUR_USER_ID: "",
+};
+
+const socialprofils = {
+    github: "https://github.com/Autokratz",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/hectorcabra/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
